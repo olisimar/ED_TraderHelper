37c22b52f2cb41094910582e1985463e172eb646
Werner Johansson <kwm.johansson@gmail.com>
2015-06-08 14:05:03 +0200

    Stupid Eclipse can't work a proper filesystem anymore.

---
+++ src/main/java/se/good_omens/EliteDangerous_TraderHelper/common/enums/COMMIDITY_TYPES.java
@@ -0,0 +1,17 @@
+package se.good_omens.EliteDangerous_TraderHelper.common.enums;
+
+/**
+ * This is to help define a commidity down to a type.
+ * Essentially all are of some type, even if it's UNKNOWN. This is to help sorting through
+ * the myriad of tradables.
+ *
+ * @author Werner
+ * @date Jun 8, 2015
+ */
+public enum COMMIDITY_TYPES {
+	UNKNOWN
+	,	AGRICULTURE
+	,	TECHNOLOGY
+	,	LEGAL_DRUGS
+	,
+}
---
+++ src/main/java/se/good_omens/EliteDangerous_TraderHelper/common/enums/COMMODITIES.java
@@ -0,0 +1,11 @@
+package se.good_omens.EliteDangerous_TraderHelper.common.enums;
+
+/**
+ * This is to represent all known base commodities.
+ *
+ * @author Werner
+ * @date Jun 8, 2015
+ */
+public enum COMMODITIES {
+
+}
