61ed5f1be8297ccf120cd9f8953bffe4a55374b1
Werner Johansson <kwm.johansson@gmail.com>
2015-06-08 20:22:51 +0200

    First real commit I hope

---
+++ src/test/java/se/good_omens/EliteDangerous_TraderHelper/tests/runTests.java
@@ -0,0 +1,11 @@
+package se.good_omens.EliteDangerous_TraderHelper.tests;
+
+import org.testng.annotations.Test;
+
+public class runTests {
+
+	@Test
+	public void testCompile() {
+		System.out.println("Running");
+	}
+}
